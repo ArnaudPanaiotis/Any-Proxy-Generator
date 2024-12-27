616061a46482c113bd2347218e43e93993866483
ArnaudPanaiotis <git.panaiotis@caramail.fr>
2024-12-27 15:09:25 +0100

    FIX: Not working from localhost

--- constants.js
+++ constants.js
@@ -8,9 +8,7 @@ const syntaxText = `Get URL of the image you want to print, right click then:
 - copy URL of the picture
 - open picture in new tab, then copy the URL
 You may prefix the URL of the card with quantity.
-You may use a file from your disk.
 
 
 https://panaiotis.fr/Any-Proxy-Generator/img/terraforming_mars_16psyche.webp
-3 https://panaiotis.fr/Any-Proxy-Generator/img/altered_scarabot_token.jpg
-2 E:\Documents\Any-Proxy-Generator\img\altered_booda_token.jpg`;
+3 https://panaiotis.fr/Any-Proxy-Generator/img/altered_scarabot_token.jpg`;
